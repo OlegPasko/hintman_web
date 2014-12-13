e763c6665ad68277e9491f199a00a654bce79afc
Oleg Pasko <olegpasko@gmail.com>
2014-12-13 18:20:57 +0200

    group show

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -6,7 +6,14 @@ class Api::V1::GroupsController < ApplicationController
     groups = Group.all
 
     respond_to do |format|
-      format.json { render json: { 'groups' => groups } }
+      format.json { render json: { 'groups' => groups }, except: [:created_at, :updated_at] }
+    end
+  end
+
+  def show
+    group = Group.find(params[:id])
+    respond_to do |format|
+      format.json { render json: group, except: [:created_at, :updated_at], include: [ :hints => { except: [:created_at, :updated_at, :group_id, :user_id] } ] }
     end
   end
 end
