708b5cd23063d16a7aea593ba989c5db5e1d5fee
Oleg Pasko <olegpasko@gmail.com>
2014-12-13 16:31:06 +0200

    routes and groups

---
+++ app/controllers/api/v1/groups_controller.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+class Api::V1::GroupsController < ApplicationController
+  before_action :set_access_control_headers
+
+  def index
+    groups = Group.all
+
+    respond_to do |format|
+      format.json { render json: groups }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#root'
+
+  namespace :api do
+    namespace :v1 do
+      resources :groups, only: [:index, :show]
+    end
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
