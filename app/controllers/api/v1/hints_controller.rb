26aab272e1474ad51490c987a4b39ae119f40513
Oleg Pasko <olegpasko@gmail.com>
2014-12-14 02:36:58 +0200

    hint create

--- app/controllers/api/v1/hints_controller.rb
+++ app/controllers/api/v1/hints_controller.rb
@@ -1,26 +1,39 @@
 # encoding: utf-8
 class Api::V1::HintsController < ApplicationController
   before_action :set_access_control_headers
-  before_action :check_token, only: [:vote]
+  before_action :check_token, only: [:vote, :create]
 
   def show
     hint = Hint.find(params[:id])
 
-    expired = hint.created_at < Time.now-1.day ? true : false
-
-    voted = if params[:user_id].present?
-              UserVote.where(user_id: params[:user_id].to_i, hint: hint).any? ? true : false
-            else
-              false
-            end
-
-    vote_value = if voted
-                   UserVote.where(user_id: params[:user_id].to_i, hint: hint).first.positive ? 'up' : 'down'
-                 else
-                   false
-                 end
+    expired, vote_value, voted = voting_data(hint)
     respond_to do |format|
       format.json { render json: hint.as_json(include: { group: { only: [:name] } }).merge(expired: expired, voted: voted, vote_value: vote_value) }
+    end
+  end
+
+  def create
+    if params[:user_id].present? && params[:group_id].present? && params[:content].present?
+      hint = Hint.new
+      hint.group = Group.find(params[:group_id])
+      hint.user = User.find(params[:user_id])
+      hint.content = params[:content]
+      if hint.save
+        expired, vote_value, voted = voting_data(hint)
+        respond_to do |format|
+          format.json { render json: hint.as_json(include: { group: { only: [:name] } }).merge(expired: expired, voted: voted, vote_value: vote_value) }
+        end
+      else
+        respond_to do |format|
+          format.json { render json: {errors: user_vote.errors.full_messages }, status: 422 }
+        end
+      end
+
+
+    else
+      respond_to do |format|
+        format.json { render json: {errors: ['Parameters missing.'] }, status: 422 }
+      end
     end
   end
 
@@ -31,15 +44,7 @@ class Api::V1::HintsController < ApplicationController
       user_vote = UserVote.new(user_id: params[:user_id], positive: positive, hint: hint)
       if user_vote.save
 
-        expired = hint.created_at < Time.now-1.day ? true : false
-
-        voted = UserVote.where(user_id: params[:user_id].to_i, hint: hint).any? ? true : false
-
-        vote_value = if voted
-                       UserVote.where(user_id: params[:user_id].to_i, hint: hint).first.positive ? 'up' : 'down'
-                     else
-                       false
-                     end
+        expired, vote_value, voted = voting_data(hint)
 
         respond_to do |format|
           format.json { render json: hint.as_json(include: { group: { only: [:name] } }).merge(expired: expired, voted: voted, vote_value: vote_value) }
@@ -55,6 +60,25 @@ class Api::V1::HintsController < ApplicationController
       end
     end
 
+  end
+
+  private
+
+  def voting_data(hint)
+    expired = hint.created_at < Time.now-1.day ? true : false
+
+    voted = if params[:user_id].present?
+              UserVote.where(user_id: params[:user_id].to_i, hint: hint).any? ? true : false
+            else
+              false
+            end
+
+    vote_value = if voted
+                   UserVote.where(user_id: params[:user_id].to_i, hint: hint).first.positive ? 'up' : 'down'
+                 else
+                   false
+                 end
+    return expired, vote_value, voted
   end
 
 end
--- app/models/hint.rb
+++ app/models/hint.rb
@@ -3,7 +3,9 @@ class Hint < ActiveRecord::Base
   belongs_to :user
   has_many :user_votes, dependent: :destroy
 
-  validates_presence_of :user, :group
+  validates_presence_of :user, :group, :content
+
+  validates_length_of :content, maximum: 500
 
   scope :active, -> { where('created_at > ?', Time.now-1.day) }
 
